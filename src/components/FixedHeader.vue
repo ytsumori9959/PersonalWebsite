455ccbb06b1e286667863a7ace591d62885fd474
ytsumori9959 <yutatsumori@gmail.com>
2020-02-09 01:04:57 -0700

    Add FixedHeader.vue

---
+++ src/components/FixedHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar dense absolute>
+    <v-spacer />
+    <v-toolbar-title>Yuta Tsumori</v-toolbar-title>
+    <v-spacer />
+    <v-app-bar-nav-icon />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "FixedHeader",
+
+  components: {},
+
+  data: () => ({
+    //
+  })
+};
+</script>

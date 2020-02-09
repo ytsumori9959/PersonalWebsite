48d16573801fd0515c0f8f21a97756047636027e
ytsumori9959 <yutatsumori@gmail.com>
2020-02-09 05:11:28 -0700

    Add cards in Work.vue

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,7 +1,38 @@
 <template>
   <div>
     <p class="display-3 text-sm-left font-weight-black">WORK</p>
-    <p class="text-sm-left">Left aligned text on viewports sized SM (small) or wider for rtl or ltr.</p>
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -12,7 +43,25 @@ export default {
   components: {},
 
   data: () => ({
-    //
+    cards: [
+      {
+        title: "HOMEPAGE",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 4
+      },
+      {
+        title: "SPREAD SHEET",
+        src:
+          "https://aquaticinformatics.com/wp-content/uploads/2017/06/Excel-Spreadsheet.png",
+        flex: 4
+      },
+      {
+        title: "SUDOKU SOLVER",
+        src:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg/1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png",
+        flex: 4
+      }
+    ]
   })
 };
 </script>

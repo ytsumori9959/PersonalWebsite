4c505bade6b649dfb9cd1d59ebe92ef0986fc264
ytsumori9959 <yutatsumori@gmail.com>
2020-03-02 21:40:54 -0700

    Complete website

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="ml-3 mt-5 mr-8">
     <p class="headline text-sm-left font-weight-bold">ABOUT</p>
-    <p class="text-sm-left pl-3 mt-n2">
+    <v-img class="ml-3" :src="profileImage" width="200" height="200" />
+    <p class="text-sm-left pl-3 mt-4">
       津守 勇汰（つもり ゆうた）
       <br />
       <br />2012年、中学1年生の頃に親の転勤でアメリカ合衆国、カリフォルニア州サンノゼに移住。現地の中学校を卒業後、高校での選択科目にプログラミング授業を選択しプログラミングに興味を持つ。2017年に現地の高校(Lynbrook High School)を卒業。現在、ユタ州ソルトレイクシティのユタ大学(The University of Utah)コンピュータサイエンス学科に在学。
@@ -20,8 +21,10 @@
       <br />
       <br />
       <strong>【経験】</strong>
-      <br />2018年6月～2018年8月 ダイビングチームうなりざき アルバイト
-      <br />2019年7月～2019年8月 Siggraph 2019 Student Volunteer
+      <br />2018年6月～2018年8月
+      <a href="http://www.unarizaki.com/iriomote/" target="_blank">ダイビングチームうなりざき西表</a> アルバイト
+      <br />2019年7月～2019年8月
+      <a href="https://s2019.siggraph.org/" target="_blank">Siggraph 2019</a> Student Volunteer
       <br />
       <br />
       <strong>【興味】</strong>
@@ -38,11 +41,15 @@
 </template>
 
 <script>
+import ProfileImage from "@/assets/FB_IMG_1564662521061.jpg";
+
 export default {
   name: "About",
 
   components: {},
 
-  data: () => ({})
+  data: () => ({
+    profileImage: ProfileImage
+  })
 };
 </script>
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="ml-3 mt-5">
+  <div class="ml-3 mt-10">
     <p class="headline text-sm-left font-weight-bold">WORK</p>
-    <v-container fluid fill-height>
+    <v-container fluid fill-height class="mt-n6">
       <v-row justify="center">
         <v-col v-for="card in cards" :key="card.title">
           <v-card max-width="500px" min-width="300px" class="grey lighten-1">
-            <v-card-title v-text="card.title"></v-card-title>
-            <v-card-text v-text="card.description"></v-card-text>
-            <v-card-subtitle class="mt-n4">・目的、役割</v-card-subtitle>
-            <v-card-text class="mt-n4" v-text="card.purpose"></v-card-text>
-            <v-card-subtitle class="mt-n4">・主なスキル</v-card-subtitle>
-            <v-card-text class="mt-n4" v-text="card.skills"></v-card-text>
-            <v-card-subtitle
-              class="mt-n4"
-              v-if="card.isCodePrivate"
-            >※大学授業内での制作物のため、ソースコードの公開は行っていません</v-card-subtitle>
+            <v-card-title v-text="card.title" class="black--text"></v-card-title>
+            <v-card-text v-text="card.description" class="black--text"></v-card-text>
+            <v-card-subtitle class="mt-n4 black--text">・目的、役割</v-card-subtitle>
+            <v-container class="py-0 my-0" v-for="purpose in card.purposes" :key="purpose">
+              <v-card-text class="mt-n7" v-text="purpose"></v-card-text>
+            </v-container>
+            <v-card-subtitle class="mt-n2 black--text">・主なスキル</v-card-subtitle>
+            <v-card-text class="ml-3 mt-n2 black--text" v-text="card.skills"></v-card-text>
+            <v-card-text class="mt-n4" v-if="card.isCodePrivate">※大学授業内での制作物のため、ソースコードの公開は行っていません</v-card-text>
 
             <v-card-actions class="mt-n5">
               <v-spacer />
-
               <v-btn icon :href="card.launchLink" target="_blank">
                 <v-icon>{{launchPath}}</v-icon>
               </v-btn>
 
-              <v-btn icon :href="card.codeLink" target="_blank">
+              <v-btn v-if="!card.isCodePrivate" icon :href="card.codeLink" target="_blank">
                 <v-icon>{{codeTagPath}}</v-icon>
               </v-btn>
             </v-card-actions>
@@ -45,44 +43,53 @@ export default {
   data: () => ({
     cards: [
       {
+        title: "Ataenakier",
+        launchLink: "https://ataenakier.firebaseapp.com/",
+        codeLink: "https://github.com/ytsumori9959/Ataenakier",
+        description:
+          "有名サイト「Akinator」に類似したゲームを複数人(3～5人)で遊べる、チャット型webアプリケーション",
+        purposes: [
+          "nuxt.jsを使用したwebアプリケーション開発",
+          "Firebaseを利用したチャットアプリケーション開発"
+        ],
+        skills: "nuxt.js, firebase",
+        isCodePrivate: false
+      },
+      {
         title: "Portfolio Website",
         launchLink: "https://yutatsumori.com/",
         codeLink: "https://github.com/ytsumori9959/PersonalWebsite.git",
         description:
           "自身のスキルや今までに作成してきたアプリケーションを紹介するためのポートフォリオウェブサイト",
-        purpose: "vue.jsを利用した簡単なウェブサイト開発",
+        purposes: ["vue.jsを利用した簡単なウェブサイト開発"],
         skills: "vue.js, vuetify.js",
         isCodePrivate: false
       },
       {
-        title: "Ataenakier",
-        launchLink: "https://ataenakier.firebaseapp.com/",
-        codeLink: "",
+        title: "Cannon Blaster -Educational App-",
+        launchLink:
+          "https://drive.google.com/open?id=1fSkWFxq36HjxVAfQ6VoSD0oqUg214pQB",
         description:
-          "Akinatorを対人で遊べるようにと、趣味で制作したwebアプリケーション",
-        purpose: "nuxt.jsとfirebaseを利用したwebアプリケーション開発",
-        skills: "nuxt.js, firebase",
-        isCodePrivate: false
-      },
-      {
-        title: "Educational App",
-        launchLink: "",
-        codeLink: "",
-        description:
-          "Qtを利用してグループ制作した教育用デスクトップアプリケーション",
-        purpose:
-          "gitのバージョン管理を利用したグループプロジェクト(6名)開発　Qtを利用したUIの作成　外部ライブラリ(Box2D)を利用した物理演算",
+          "簡単なゲームを遊びながら学べる教育用デスクトップアプリケーション",
+        purposes: [
+          "gitのバージョン管理を利用したグループプロジェクト(6人)開発",
+          "Qtを利用したUIの作成",
+          "外部ライブラリ(Box2D)を利用した物理演算"
+        ],
         skills: "C++, git, Qt",
         isCodePrivate: true
       },
       {
-        title: "SpreadSheet",
-        launchLink: "",
-        codeLink: "",
+        title: "Spreadsheet",
+        launchLink:
+          "https://drive.google.com/open?id=1ZCaVD4GeWxGxzXNLhMjKLDqdhnky_DR6",
         description:
-          "Qtを利用してグループ制作した学習用デスクトップアプリケーション",
-        purpose:
-          "gitのバージョン管理を利用したペアプロジェクト開発　MVCのデザインパターンを利用したアプリケーション開発　",
+          "Visual Studioを使用し、Microsoft Spreadsheetを簡易化したデスクトップアプリケーション",
+        purposes: [
+          "gitのバージョン管理を利用したペア(2人)プロジェクト開発",
+          "MVCのデザインパターンを利用したアプリケーション開発",
+          "Visual Studioを利用したUIの作成"
+        ],
         skills: "C#, git, Visual Studio",
         isCodePrivate: true
       }
